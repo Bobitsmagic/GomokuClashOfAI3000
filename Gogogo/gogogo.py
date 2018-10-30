08fb568eda8e12e3385f2606fd9185b0eaa5797a
Bryan A. Tremblay <bryanandhallie@gmail.com>
2018-10-30 15:08:13 -0400

    Added ability for gogogo to monitor savefile for updates. Very crude and needs reworking

--- Gogogo/gogogo.py
+++ Gogogo/gogogo.py
@@ -10,19 +10,35 @@ W = "W"
 board = Board()
 b = AI(board, B)
 w = AI(board, W)
+ais = [b, w]
+
+saveFile = "../../game.gom"
+with open(saveFile, "w") as fh: fh.write("")
 
 turn = 0
-turns = 3
+turns = -1
 winner = None
-while winner == None and turn < turns:
-    for ai in [b, w]:
+while winner == None and (turn < turns or turns == -1):
+    for ai in ais:
+        oldState = open(saveFile, "r").read()
+
         ai.move()
         ai.board.render()
         print()
 
+        fh = open(saveFile, "w")
+        fh.write(board.history)
+        fh.close()
+
         if ai.board.winner != None:
             winner = ai
             break
+
+        stateChanged = False
+        while stateChanged is False:
+            newState = open(saveFile, "r").read()
+            if oldState != newState:
+                stateChanged = True
 
     turn += 1
 
