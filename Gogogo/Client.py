8b76925c835c3a3b92ce24284bb1c5792a9918ef
Bryan Tremblay <bryanandhallie@gmail.com>
2018-10-31 15:15:44 -0400

    Many additional changes to server and client

--- Gogogo/Client.py
+++ Gogogo/Client.py
@@ -3,6 +3,7 @@ from twisted.internet import reactor, protocol
 import json, time, random
 
 port = 4321
+verbose = 1
 
 class Client(protocol.Protocol):
     def connectionMade(self):
@@ -10,37 +11,65 @@ class Client(protocol.Protocol):
         self.registered = False
         self.register()
 
+        self.inMatch = False
+        self.myStone = None
+        self.opponentStone = None
+
     def dataReceived(self, data):
-        print("Got\n\t" + str(data))
         # [TODO] Find a proper place to close this out
         #self.transport.loseConnection()
+
+        try:
+            data = data.decode()
+            data = json.loads(data)
+            if verbose > 1: print("Received\n\t" + str(data))
+
+            if "query" in data:
+                self.processQuery(data)
+        except:
+            print("Recieved incorrect JSON\n\t" + str(data))
+        finally:
+            pass
+
+    def sendData(self, data):
+        if verbose > 1: print("Sending\n\t" + str(data))
+        data = json.dumps(data)
+        data = data.encode()
+        self.transport.write(data)
+
+    def processQuery(self, data):
+        query = data["query"]
+
+        if query == "register":
+            self.processQuery_register(data)
+        if query == "match":
+            self.processQuery_match(data)
 
     def connectionLost(self, reason):
         print("Connection lost")
 
     def register(self):
-        message = json.dumps({"query": "register", "username": self.username})
-        self.transport.write(message.encode())
+        if self.registered:
+            print("[ERROR] Cannot register twice!")
+            return
 
-        """
-        data = self.getData()
-        if data == None:
-            return False
+        self.sendData({"query": "register", "username": self.username})
 
-        if "query" not in data:
-            if self.verbose:
-                print("Failed to receive proper reply from server:\n\t" + str(data))
-            return False
+    def processQuery_register(self, data):
+        username = data["username"]
+        if username != self.username: return
+        state = data["state"]
+        if state != "success": return
 
-        if data["query"] == "register" and data["state"] == "success":
-            if self.verbose:
-                print("Registered user " + self.username)
-            self.registered = True
-        else:
-            if self.verbose:
-                print("Unable to register user " + self.username)
-            return False
-        """
+        self.registered = True
+        print("Registered as: " + username)
+
+    def processQuery_match(self, data):
+        self.inMatch = True
+        self.opponent = data["opponent"]
+        self.myStone = data[self.username]
+        self.opponentStone = data[self.opponent]
+        print("Entered match ({}) against player {} ({})".format(self.myStone, self.opponent, self.opponentStone))
 
 class Factory(protocol.ClientFactory):
     protocol = Client
--- server.py
+++ server.py
@@ -1,25 +1,61 @@
 from twisted.internet import reactor, protocol
-import json
+import json, time, threading
 
 # [TODO] Figure out if this is the proper global place to place these
-verbose = False
+verbose = 1
 activePlayers = []
 lobbyPlayers = []
 activeMatches = []
 
-def findMatch():
-    print("Attempting to find a match")
-    if len(lobbyPlayers) >= 2:
-        a = lobbyPlayers[0]
-        b = lobbyPlayers[1]
-        lobbyPlayers.remove(a)
-        lobbyPlayers.remove(b)
-        activePlayers.append(a)
-        activePlayers.append(b)
-        createMatch(a, b)
+sockets = {}
 
-def createMatch(playerA, playerB):
-    print("Match started between {} and {}".format(playerA, playerB))
+class Match(object):
+    def __init__(self, playerA, playerAStone, playerB, playerBStone):
+        sockets[playerA].createMatch(playerA, playerAStone, playerB, playerBStone, playerB)
+        sockets[playerB].createMatch(playerA, playerAStone, playerB, playerBStone, playerA)
+
+        self.playerA = playerA
+        self.playerAStone = playerAStone
+        self.playerB = playerB
+        self.playerBStone = playerBStone
+        if self.playerAStone == "B": self.currentPlayer = self.playerA
+        if self.playerBStone == "B": self.currentPlayer = self.playerB
+
+        thread = threading.Thread(target = self.run, args = ())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+        print("Match started!!!")
+
+class MatchmakingSystem(object):
+    def __init__(self, interval = 0.5):
+        self.interval = interval
+
+        thread = threading.Thread(target = self.run, args = ())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+        print("Matchmaking System has started searching for matches at interval of {} seconds".format(self.interval))
+        while True:
+            self.findMatch()
+            time.sleep(self.interval)
+
+    def findMatch(self):
+        if len(lobbyPlayers) >= 2:
+            playerA = lobbyPlayers[0]
+            playerB = lobbyPlayers[1]
+            lobbyPlayers.remove(playerA)
+            lobbyPlayers.remove(playerB)
+            activePlayers.append(playerA)
+            activePlayers.append(playerB)
+
+            print("Match found between {} and {}".format(playerA, playerB))
+            # [TODO] Make this randomized
+            playerAStone = "B"
+            playerBStone = "W"
+            match = Match(playerA, playerAStone, playerB, playerBStone)
 
 class Server(protocol.Protocol):
     def dataReceived(self, data):
@@ -28,7 +64,7 @@ class Server(protocol.Protocol):
         try:
             data = data.decode()
             data = json.loads(data)
-            if verbose: print("Received\n" + str(data))
+            if verbose > 1: print("Received\n\t" + str(data))
 
             if "query" in data:
                 if data["query"] == "register":
@@ -37,38 +73,44 @@ class Server(protocol.Protocol):
             print("Recieved incorrect JSON\n\t" + str(data))
         finally:
             if reply == None:
-                reply = json.dumps({"response": "bad"})
+                reply = {"response": "bad"}
 
             self.sendData(reply)
 
     def sendData(self, data):
-        if verbose: print("Sending\n\t" + data)
-        self.transport.write(data.encode())
+        if verbose > 1: print("Sending\n\t" + str(data))
+        data = json.dumps(data)
+        data = data.encode()
+        self.transport.write(data)
 
     def processQuery(self, data):
-        action = data["query"]
+        query = data["query"]
 
-        if action == "register":
+        if query == "register":
             return self.processQuery_register(data)
 
         return None
 
     def processQuery_register(self, data):
         username = data["username"]
-        if verbose: print("User attempting to register with name " + username)
+        if verbose > 1: print("User attempting to register with name " + username)
         if username not in activePlayers and username not in activePlayers:
             lobbyPlayers.append(username)
+            sockets[username] = self
             print("User Registered: " + username)
-            findMatch()
-            return json.dumps({"query": "register", "username": username, "state": "success"})
+            return {"query": "register", "username": username, "state": "success"}
         else:
-            return json.dumps({"query": "register", "username": username, "state": "fail"})
+            return {"query": "register", "username": username, "state": "fail"}
 
         return None
+
+    def createMatch(self, playerA, playerAStone, playerB, playerBStone, opponent):
+        self.sendData({"query" : "match", playerA : playerAStone, playerB : playerBStone, "opponent" : opponent})
 
 def main():
     port = 4321
     print("Starting server on port {}".format(port))
+    matchmakingSystem = MatchmakingSystem()
 
     factory = protocol.ServerFactory()
     factory.protocol = Server
