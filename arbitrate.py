073be8220a314bad3024b84374a9cc5e95c85ad7
Bryan Tremblay <bryanandhallie@gmail.com>
2018-10-30 20:05:14 -0400

    Added game ability to arbiter

--- arbitrate.py
+++ arbitrate.py
@@ -7,90 +7,71 @@ import subprocess
 from Gogogo.Board import Board
 from Gogogo.AI import AI
 
-saveFile = "../game.gom"
-saveFile = os.path.abspath(saveFile)
+gameCount = 10
+for game in range(gameCount):
+    saveFile = "../game.gom"
+    saveFile = os.path.abspath(saveFile)
 
-with open(saveFile, "w") as fh: fh.write("")
+    with open(saveFile, "w") as fh: fh.write("")
 
-# [TODO] Make this randomly select between stone colors instead of hardcoded
-gogogo_board = Board()
-#gogogo = AI(gogogo_board, "B", saveFile = saveFile, verbose = True)
-gogogo = AI(gogogo_board, "B", saveFile = saveFile, verbose = True)
-#HansAI = AI(gogogo_board, "W", saveFile = saveFile)
+    # [TODO] Make this randomly select between stone colors instead of hardcoded
+    gogogo_board = Board()
+    #gogogo = AI(gogogo_board, "B", saveFile = saveFile, verbose = True)
+    gogogo = AI(gogogo_board, "B", saveFile = saveFile, verbose = True)
+    #HansAI = AI(gogogo_board, "W", saveFile = saveFile)
 
-HansAI_loc = os.path.abspath("./HansAI/bin/Release/Gomoku.exe")
-difficulty = 100
-command = HansAI_loc + " " + saveFile + " " + "W" + " " + str(difficulty)
-process = subprocess.Popen(command, shell = True)
-HansAI = None
+    HansAI_loc = os.path.abspath("./HansAI/bin/Release/Gomoku.exe")
+    difficulty = 10
+    command = HansAI_loc + " " + saveFile + " " + "W" + " " + str(difficulty)
+    process = subprocess.Popen(command, shell = True)
+    HansAI = None
 
-b = gogogo
-w = HansAI
+    b = gogogo
+    w = HansAI
 
-turn = 0
-turns = -1
-winner = None
-while winner == None and (turn < turns or turns == -1):
-    gameState = open(saveFile, "r").read()
+    turn = 0
+    turns = -1
+    winner = None
+    winnerCode = ""
+    while winner == None and (turn < turns or turns == -1):
+        gameState = open(saveFile, "r").read()
 
-    """
-    start = os.stat(saveFile).st_mtime
-    if b is not None: b.move()
-    playerMoved = False
-    while playerMoved == False:
-        now = os.stat(saveFile).st_mtime
-        if now - start != 0: playerMoved = True
-    with open(saveFile, "r") as fh: gameState = fh.read()
-    if b is not None:
+        # [TODO] Make this work elegantly so that we can randomize B/W initializing
+
+        b.move()
+        with open(saveFile, "r") as fh: gameState = fh.read()
+
+        start = os.stat(saveFile).st_mtime
+        playerMoved = False
+        while playerMoved == False:
+            try:
+                now = os.stat(saveFile).st_mtime
+            except Exception:
+                pass
+            if now - start != 0: playerMoved = True
+
+        with open(saveFile, "r") as fh: gameState = fh.read()
         board = Board()
         board.processCode(gameState)
         b.board = board
 
-    start = os.stat(saveFile).st_mtime
-    if w is not None: w.move()
-    playerMoved = False
-    while playerMoved == False:
-        now = os.stat(saveFile).st_mtime
-        if now - start != 0: playerMoved = True
-    with open(saveFile, "r") as fh: gameState = fh.read()
-    if w is not None:
-        board = Board()
-        board.processCode(gameState)
-        w.board = board
+        #board.render()
 
-    board = Board()
-    board.processCode(gameState)
-    board.render()
-    """
+        if board.winner != None:
+            winner = board.winner
+            winnerCode = board.history
+            break
 
-    # [TODO] Make this work elegantly so that we can randomize B/W initializing
-
-    b.move()
-    with open(saveFile, "r") as fh: gameState = fh.read()
-
-    start = os.stat(saveFile).st_mtime
-    playerMoved = False
-    while playerMoved == False:
-        try:
-            now = os.stat(saveFile).st_mtime
-        except Exception:
-            pass
-        if now - start != 0: playerMoved = True
-
-    with open(saveFile, "r") as fh: gameState = fh.read()
-    board = Board()
-    board.processCode(gameState)
-    b.board = board
-
-    board.render()
-
-    if board.winner != None:
-        winner = board.winner
-        break
-
-    turn += 1
-
-if winner == b:
-    print("Black is the victor!")
-else:
-    print("White is the victor!")
+        turn += 1
+    
+    scoreFile = "../score.txt"
+    scoreFile = os.path.abspath(scoreFile)
+    with open(scoreFile, "a") as fh:
+        if winner == b:
+            print("Black is the victor!")
+            fh.write("Winner B\n")
+            fh.write(winnerCode + "\n")
+        else:
+            print("White is the victor!")
+            fh.write("Winner W\n")
+            fh.write(winnerCode + "\n")
